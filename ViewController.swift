d572a3cd7a65da6d1d296e2db57ed78186f196a2
hjacobs1 <henryijacobs@gmail.com>
2017-11-10 19:54:32 -0800

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Column Quest Alpha
+//
+//  Created by Henry Jacobs on 11/1/17.
+//  Copyright © 2017 Henry Jacobs. All rights reserved.
+//
+
+import UIKit
+import EasyImagy
+import MathSwift
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imagePicked: UIImageView!
+    
+    @IBAction func openCameraButton(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            var imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func openPhotoLibraryButton(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            var imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imagePicked.image = image
+        dismiss(animated:true, completion: nil)
+    }
+
+    @IBAction func saveButt(sender: AnyObject) {
+        var imageData = UIImageJPEGRepresentation(imagePicked.image!, 0.6)
+        var compressedJPGImage = UIImage(data: imageData!)
+        UIImageWriteToSavedPhotosAlbum(compressedJPGImage!, nil, nil, nil)
+        
+        var alert = UIAlertView(title: "Wow",
+                                message: "Your image has been saved to Photo Library!",
+                                delegate: nil,
+                                cancelButtonTitle: "Ok")
+        alert.show()
+    }
+    
+    // image to be converted to matrix
+    var proc = ImageProcessor()
+    var imageView = UIImageView()
+    var matrixArray = Math()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView = UIImageView(frame: self.view.frame)
+        imageView.image = proc.image.uiImage
+        imageView.contentMode = .scaleAspectFit
+        self.view.addSubview(imageView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
